028723e7ea0705bfb09bcf42afae567d3a56ccda
HK <kirayamato25.tk@gmail.com>
2018-01-18 22:12:15 +0800

    Changed to form and query added

--- myProfile.php
+++ myProfile.php
@@ -1,4 +1,19 @@
 <!DOCTYPE html>
+<?php
+include("inc/conn.php");
+if($_SERVER['REQUEST_METHOD']== 'POST'){
+	$sql = "UPDATE member SET memberName='$_POST[memName]',memberAddress='$_POST[memAddress]',memberPhone='$_POST[memPhone]',memberCreditCard='$_POST[memCreditCard]' WHERE memberId=$userId;";
+	if (mysqli_query($con,$sql)){
+		echo'<script type="text/javascript">
+			alert("Your profile has been successfully updated.");
+			window.location.replace("myProfile.php"); 					
+			</script>';
+	}
+	else{
+		die('Error: ' . mysqli_error($conn));
+	}
+	mysqli_close($con);
+?>
 <html lang="en">
 	<head>
 	<meta charset="utf-8">
@@ -14,36 +29,37 @@
 				{  
 				while($row = mysqli_fetch_array($result))  
 				{
-			?> 
-			<div class="profileDetail">
-				<p>Name</p>
-				<p><?php echo $row["memberName"];?></p>
-				<input type="hidden">
-			</div>
-			<div class="profileDetail">
-				<p><Email-Address</p>
-				<p><?php echo $row["memberEmail"];?></p>
-				<input type="hidden">
-			</div>
-			<div class="profileDetail">
-				<p>Password</p>
-				<button onclick="location.href='changePassword.php'" type="button">Change Password</button>
-				<input type="hidden">
-			</div>
-			<div class="profileDetail">
-				<p>Mobile Phone</p>
-				<p><?php echo $row["memberPhone"];?></p>
-				<input type="hidden">
-			</div>
-			<div class="profileDetail">
-				<p>Credit Card</p>
-				<p><?php echo $row["memberCreditCard"];?></p>
-				<input type="hidden">
-			</div>			
-			<div class="profileDetail">
-				<p>Reward Point</p>
-				<p><?php echo $row["memberPoint"];?></p>
-			</div>				
+			?>
+			<form>
+				<div class="profileDetail">
+					<label>Name</label>
+					<input type="text" name="memName" value="<?php echo $row["memberName"];?>" required />
+				</div>
+				<div class="profileDetail">
+					<label><Email-Address</label>
+					<input type="text" value="<?php echo $row["memberEmail"];?>" disabled="disabled" />
+				</div>
+				<div class="profileDetail">
+					<label>Password</label>
+					<button onclick="location.href='changePassword.php'" type="button">Change Password</button>
+				</div>
+				<div class="profileDetail">
+					<label>Address</label>
+					<input type="text" name="memAddress" value="<?php echo $row["memberAddress"];?>" required />
+				</div>
+				<div class="profileDetail">
+					<label>Mobile Phone</label>
+					<input type="text" name="memPhone" value="<?php echo $row["memberPhone"];?>" required />
+				</div>
+				<div class="profileDetail">
+					<label>Credit Card</label>
+					<input type="text" name="memCreditCard" value="<?php echo $row["memberCreditCard"];?>" required/>
+				</div>			
+				<div class="profileDetail">
+					<label>Reward Point</label>
+					<input type="text value="<?php echo $row["memberPoint"];?>" disabled="disabled"/>
+				</div>
+			</form>
 			<?php  
 				}  
 				}  
