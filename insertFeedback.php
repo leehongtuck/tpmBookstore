78b502ad38d33b7e9f5a8ff537b692bdcf7db4c4
iKira25 <kirayamato25.tk@gmail.com>
2018-01-19 21:06:13 +0800

    Not working but changed

--- insertFeedback.php
+++ insertFeedback.php
@@ -1,13 +1,14 @@
 <?php
 include("inc/conn.php");
-$sql1="SELECT * FROM feedback WHERE memberId=".$userId."'";
-$result = mysqli_query($conn, $sql1);  
-if(mysqli_num_rows($result) > 0)  
+$sql1="SELECT * FROM feedback";
+$result1 = mysqli_query($conn, $sql1);  
+if(mysqli_num_rows($result1) > 0)  
 	{  
-		while($row = mysqli_fetch_array($result))  
+
+		while($row1 = mysqli_fetch_array($result1))  
 			{
-				$memberCheck=$row["memberId"];
-			}
+				$memberCheck=$row1["memberId"];
+			
 if (isset($memberCheck)){
 			 echo '<script type="text/javascript">
 					alert("You have already insert a feedback!");
@@ -15,42 +16,44 @@ if (isset($memberCheck)){
 					</script>';
 }
 else{
-if($_POST['giveFeedback']==True){
-	$rating= isset($_POST['rating']);
-	if ($rating == False) {
-		echo '<script type="text/javascript">
-				alert("Please Select a star!"); 
-				window.location.replace("bookDetail.php?id='.$_POST['hiddenBookId'].'"); 					
-				</script>';
-	}
-	else{
-		//SETTING THE FEEDBACKID VARIABLE
-		$sql="SELECT feedbackId FROM feedback ORDER BY feedbackId DESC LIMIT 1 ";
-		$query=mysqli_query($conn, $sql);
-		$feedbackId;
-		if($result=mysqli_fetch_array($query)){
-			$feedbackId=(int)substr($result[0], 1, 3)+1;
-			$feedbackId='F'.str_pad("00", 3, $feedbackId);
-		}else{
-			$feedbackId='F001'; 
-		}
-		//INSERT RECORD INTO DATABASE
-		$sql="INSERT INTO feedback (feedbackId,bookRating,bookComment,bookId,memberId,feedbackStatus)
-		VALUES
-		('$feedbackId','$_POST[rating]','$_POST[comment]','$_POST[hiddenBookId]','$userId','0')";
-		if (mysqli_query($conn,$sql)){ 
-			$lastId = mysqli_insert_id($conn);
-			$lastId = mysqli_real_escape_string($conn,$lastId);
-			 echo '<script type="text/javascript">
-					alert("Thank You For Your Feedback!");
+		echo "what";
+		$rating= isset($_POST['rating']);
+		if ($rating == False) {
+			echo '<script type="text/javascript">
+					alert("Please Select a star!"); 
 					window.location.replace("bookDetail.php?id='.$_POST['hiddenBookId'].'"); 					
 					</script>';
+		}
+		else{
+			//SETTING THE FEEDBACKID VARIABLE
+			$sql="SELECT feedbackId FROM feedback ORDER BY feedbackId DESC LIMIT 1 ";
+			$query=mysqli_query($conn, $sql);
+			$feedbackId;
+			if($result=mysqli_fetch_array($query)){
+				echo"what";
+				$feedbackId=(int)substr($result[0], 1, 3)+1;
+				$feedbackId='F'.str_pad("00", 3, $feedbackId);
+			}else{
+				$feedbackId='F001'; 
 			}
-			else{
-			 die('Error: ' . mysqli_error($conn));   
-			 mysqli_close($conn);
-			}
+			//INSERT RECORD INTO DATABASE
+			$sql="INSERT INTO feedback (feedbackId,bookRating,bookComment,bookId,memberId,feedbackStatus)
+			VALUES
+			('$feedbackId','$_POST[rating]','$_POST[comment]','$_POST[hiddenBookId]','$userId','0')";
+			if (mysqli_query($conn,$sql)){ 
+				$lastId = mysqli_insert_id($conn);
+				$lastId = mysqli_real_escape_string($conn,$lastId);
+				 echo '<script type="text/javascript">
+						alert("Thank You For Your Feedback!");
+						window.location.replace("bookDetail.php?id='.$_POST['hiddenBookId'].'"); 					
+						</script>';
+				}
+				else{
+				 die('Error: ' . mysqli_error($conn));   
+				 mysqli_close($conn);
+				}
 	}
 }
 }
+	}
 ?>
