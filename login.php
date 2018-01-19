3d6f4bca4d76367b03c14394b893f75f83053bee
Tuck <leehongtuck@gmail.com>
2018-01-19 21:36:33 +0800

    login and register functional, login not styled yet

--- login.php
+++ login.php
@@ -1,28 +1,56 @@
 <?php
-   include("inc/memberHeader.php");
-   
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $myUsername=mysqli_real_escape_string($conn,$_POST['email']);
-      $myPassword=mysqli_real_escape_string($conn,$_POST['password']);
-	  $password=password_hash($myPassword, PASSWORD_DEFAULT);
-      
-      $sql = "SELECT memberId FROM member WHERE memberEmail = '$myUsername' and memberPw = '$password'";
-      $query = mysqli_query($con,$sql);
-      $result = mysqli_fetch_array($query);
+$title="Login";
+require_once "inc/memberHeader.php";
 
-      $count = mysqli_num_rows($result);
-      // If result matched $myusername and $mypassword, table row must be 1 row
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+  // username and password sent from form 
+  $myUsername=mysqli_real_escape_string($conn,$_POST['email']);
+  $myPassword=mysqli_real_escape_string($conn,$_POST['password']);
 
-      if($count == 1) {
-          $userId = $result[0];
-        $_SESSION['loginUser'] = $userId;
-        echo '<script>alert("Login successful")</script>';
-        echo'<script>window.location="index.html";</script>';
-      }
-	  else {
-		echo '<script>alert("Your Email Address or Password is incorrect")</script>';
-	  }
-   }
+  $sql = "SELECT memberId, memberPw FROM member WHERE memberEmail = '$myUsername' ";
+  $result = mysqli_query($conn,$sql);
+  $row=mysqli_fetch_array($result);
+  $numRows = mysqli_num_rows($result);
+
+  // If result matched $myusername and $mypassword, table row must be 1 row
+  if($numRows == 1):
+    if(password_verify ($myPassword, $row[1])):
+      //session = userid
+      $_SESSION['loginUser'] = $row[0];?>
+      <script>
+        alert("Login successful");
+        window.location.replace("index.php");
+      </script>
+    <?php
+    else:?>
+      <script>
+        alert("Your Email Address or Password is incorrect");
+      </script>
+    <?php
+    endif;
+  else:?>
+    <script>
+      alert("Your Email Address or Password is incorrect");
+    </script>
+  <?php
+  endif; 
+  } 
 ?>
+
+<form method="post">
+			<div class="inputGroup" >
+				<input name="email" type="text" required>
+				<span class="inputHighlight"></span>
+				<span class="inputBar"></span>
+				<label for="name">Email</label>	
+			</div>
+			<div class="inputGroup">
+				<input name="password" type="password" required>
+				<span class="inputHighlight"></span>
+				<span class="inputBar"></span>
+				<label>Password</label>	
+      </div>
+      <div>
+				<input class="btn" id="loginBtn" type="submit" value="Login">
+			</div>
+</form>
--- register.php
+++ register.php
@@ -1,38 +1,57 @@
 <?php 
 $title="Registration";
 require_once "inc/memberHeader.php";
+
 if($_SERVER['REQUEST_METHOD']=='POST'){
-    //SETTING THE MEMBERID VARIABLE
-    $sql="SELECT memberId FROM member ORDER BY memberId DESC LIMIT 1 ";
-    $query=mysqli_query($conn, $sql);
-    $memberId;
-    if($result=mysqli_fetch_array($query)){
-        $memberId=(int)substr($result[0], 1, 3)+1;
-        $memberId='M'.str_pad("00", 3, $memberId);
-    }else{
-        $memberId='M001'; 
-    }
+	if(!($_POST['password']===$_POST['cfmPassword'])):?>
+		<script>
+			alert("Passwords do not match!");
+		</script>
+	<?php
+	else:
+		//FINDING ANY EMAIL DUPLICATES
+		$sql="SELECT memberId FROM member WHERE memberEmail='$_POST[email]'";
+		$result=mysqli_query($conn,$sql);
+		$numRows=mysqli_num_rows($result);
+		$msg='';
+		if($numRows==1)
+			$msg.= 'Duplicate email';
 
-    //INSERT RECORD INTO DATABASE
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    $sql="INSERT INTO member (memberId,memberEmail,memberName,memberPw,memberAddress,memberPhone,memberCreditCard)
-    VALUES
-    ('$memberId','$_POST[email]','$_POST[memName]', '$password', '$_POST[memAddress]', '$_POST[memPhone]', '$_POST[memCc]')";
-    if (mysqli_query($conn,$sql)){
-        echo "Registration successful";
-        header ("Location:index.php");    
-    }else  
-        die('Error: ' . mysqli_error($conn)); 
+		else{
+			//SETTING THE MEMBERID VARIABLE
+			$sql="SELECT memberId FROM member ORDER BY memberId DESC LIMIT 1 ";
+			$result=mysqli_query($conn, $sql);
+			$memberId;
+			if($row=mysqli_fetch_array($result))
+				$memberId=++$row[0];
+			else
+				$memberId='M001'; 
 
-    mysqli_close($conn); 
+			//INSERT RECORD INTO DATABASE
+			$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+			$sql="INSERT INTO `member` (`memberId`, `memberEmail`, `memberName`, `memberPw`, `memberAddress`, `memberPhone`, `memberCreditCard`)
+			VALUES
+			('$memberId','$_POST[email]','$_POST[memName]', '$password', '$_POST[memAddress]', '$_POST[memPhone]', '$_POST[memCc]')";
+			if (mysqli_query($conn,$sql)):
+				mysqli_close($conn);?>
+				<script>
+					alert("Registration Successful!");
+					window.location.replace("login.php");
+				</script>
+			<?php else:
+				die('Error: ' . mysqli_error($conn)); 
+				mysqli_close($conn); 
+			endif;
+		}
+	endif;
 }
 ?>
 	<section id="registerContainer">
 		<div id="registerHeader">
 			<h1>Registration</h1>
 		</div>
-		<form>
-			<div class="inputGroup">
+		<form method="post">
+			<div class="inputGroup" >
 				<input name="memName" type="text" required>
 				<span class="inputHighlight"></span>
 				<span class="inputBar"></span>
@@ -49,6 +68,12 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 				<span class="inputHighlight"></span>
 				<span class="inputBar"></span>
 				<label>Password</label>	
+			</div>
+			<div class="inputGroup">
+				<input name="cfmPassword" type="password" required>
+				<span class="inputHighlight"></span>
+				<span class="inputBar"></span>
+				<label>Confirm Password</label>	
 			</div>
 			<div class="inputGroup">
 				<input name="memAddress" type="text" required>
@@ -69,7 +94,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 				<label>Credit Card Number</label>
 				</div>
 			</div>
-			<div class="input">
+			<div>
 				<input class="btn" id="registerBtn" type="submit" value="Register">
 			</div>
 		</form>
