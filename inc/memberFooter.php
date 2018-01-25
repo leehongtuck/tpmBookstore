4681afd78b0b82ac11d54bfc86c56823295702bc
JingWeiAPU <lizwei98@gmail.com>
2018-01-25 10:20:44 +0800

    Footer

---
+++ inc/memberFooter.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<title>footer</title> 
+	  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+      <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/member.css">
+</head>
+
+<body> 
+
+<footer>  
+
+<div id="fLogo">
+<img src="../img/tpmlogo.png" alt="">
+</div> 
+
+<div id="fAdd">
+<p>Technology Park Malaysia
+<br>
+Level 5, Enterprise 4 
+<br>
+Technology Park Malaysia
+<br>
+57000 Bukit Jalil, Kuala Lumpur</p>
+</div>
+
+
+
+
+</footer>
+
+</body>
+
+</html>
