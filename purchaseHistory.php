975d10d1d8745610a567df0767a1e66ecec59f13
JingWeiAPU <lizwei98@gmail.com>
2018-01-31 17:40:43 +0800

    view purchase history

--- purchaseHistory.php
+++ purchaseHistory.php
@@ -1,6 +1,7 @@
 <?php 
 $title = "Activity Log";
-require_once "inc/memberHeader.php";
+require_once "inc/memberHeader.php"; 
+$result=mysqli_query($conn,"SELECT * FROM bookPurchase INNER JOIN book ON bookpurchase.bookId=book.bookId INNER JOIN payment ON bookpurchase.paymentId=payment.paymentId ");
 /*if($member == null)
 	header('location:index.php');*/
 ?> 
@@ -22,21 +23,47 @@ require_once "inc/memberHeader.php";
 <h1>Purchase History</h1>
 <br>
 <br>
-<form action="" method="post"> 
-	
+
+
 	<div>
-	<table style="width: 100%;text-align:center">
+	<table style="width: 100%;text-align:center"> 
+	
+	
 			<tr>
 				<td class="rightBorder">Date/Time</td> 
 				<td class="rightBorder">Book Name</td>
 				<td class="bottomBorder">Book Quantity</td>
 			</tr>
 			
+<?php
+while($row=mysqli_fetch_array($result))
+{ 
+echo"<tr>";
+
+echo"<td>";
+echo$row['paymentDateTime'];
+echo"</td>"; 
+
+echo"<td>";
+echo$row['bookTitle'];
+echo"</td>"; 
+
+echo"<td>";
+echo$row['purchaseQuantity'];
+echo"</td>";
+
+echo"</tr>"; 
+}
+mysqli_close($conn);
+
+?>
+
 		</table>
 	
 	</div>
-</form>
-</div> 
+</div>  
+
+
 
 
 </body>
