684d3cc7b2d894faed4413e1f5ff14c50a58fc2f
JingWeiAPU <lizwei98@gmail.com>
2018-01-31 14:59:08 +0800

    Background

--- activity_log.php
+++ activity_log.php
@@ -1,18 +1,18 @@
 <?php 
 $title = "Activity Log";
 require_once "inc/memberHeader.php";
-if($member == null)
-	header('location:index.php');
+/*if($member == null)
+	header('location:index.php');*/
 ?> 
 
-<<<<<<< HEAD
+
 
 <!DOCTYPE html>
 <html>
 
 <head>
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
-<title>Activity log</title>
+<title>Purchase History</title>
 
 </head>
 
@@ -23,34 +23,13 @@ if($member == null)
 <br>
 <br>
 <form action="" method="post"> 
-=======
-<div id="activity">
-<form action="" method="post">
-	<table style="width: 100%">
-		<tr>
-			<td><select name="SearchDate" id="filterBtn">
-			<option>Select Date</option>
-			</select>
-			</td>
-			
-			<td><select name="SearchType" id="filterBtn">
-			<option>Select Type of Activity</option>
-			</select>
-			</td>
-			
-			
-            </form>
-            </td>		
-       </tr>
-	</table>
->>>>>>> d69950a60df3ef93210bcd26936d477b97a3c287
 	
 	<div>
 	<table style="width: 100%;text-align:center">
 			<tr>
-				<td class="rightBorder">Date</td>
-				<td class="rightBorder">Type</td>
-				<td class="bottomBorder">Activity Description</td>
+				<td class="rightBorder">Date/Time</td> 
+				<td class="rightBorder">Book Name</td>
+				<td class="bottomBorder">Book Quantity</td>
 			</tr>
 			
 		</table>
