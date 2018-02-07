1e484535b6ed36f941abca2074edf15f484bc4b9
Lee Hong Tuck <leehongtuck@gmail.com>
2018-02-07 09:35:54 +0800

    sql latest

--- tpmbookstore.sql
+++ tpmbookstore.sql
@@ -2,10 +2,10 @@
 -- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 06, 2018 at 01:39 PM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.2.0
+-- Host: 127.0.0.1:3308
+-- Generation Time: Feb 07, 2018 at 01:21 AM
+-- Server version: 5.7.19
+-- PHP Version: 7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,16 +28,19 @@ SET time_zone = "+00:00";
 -- Table structure for table `book`
 --
 
-CREATE TABLE `book` (
+DROP TABLE IF EXISTS `book`;
+CREATE TABLE IF NOT EXISTS `book` (
   `bookId` varchar(10) NOT NULL,
   `bookTitle` varchar(255) NOT NULL,
   `bookDescription` varchar(255) NOT NULL,
   `bookPrice` double NOT NULL,
   `bookAuthor` varchar(50) NOT NULL,
   `bookPublisher` varchar(50) NOT NULL,
-  `bookPublishDate` date NOT NULL,
+  `bookPublishDate` varchar(50) NOT NULL,
   `bookQuantity` int(11) NOT NULL,
-  `genreId` varchar(4) NOT NULL
+  `genreId` varchar(4) NOT NULL,
+  PRIMARY KEY (`bookId`),
+  KEY `book_ibfk_1` (`genreId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -45,15 +48,15 @@ CREATE TABLE `book` (
 --
 
 INSERT INTO `book` (`bookId`, `bookTitle`, `bookDescription`, `bookPrice`, `bookAuthor`, `bookPublisher`, `bookPublishDate`, `bookQuantity`, `genreId`) VALUES
-('B001', 'Milk and Honey', 'It is about the experience of violence, abuse, love, loss, and femininity.', 20, 'Rupi Kaur', 'Createspace', '2014-07-09', 10, 'G001'),
-('B002', 'Pride and Prejudice', 'It is a truth universally acknowledged, that a single man in possession of a good fortune must be in want of a wife.  So begins Pride and Prejudice, Jane Austenï¿½s witty comedy of manners', 30, ' Jane Austen', 'Modern Library', '2016-09-25', 13, 'G001'),
-('B003', 'Memories of Geisha', 'A literary sensation and runaway bestseller, this brilliant debut novel presents with seamless authenticity and exquisite.', 15, 'Arthur Golden', 'Random House Large Print Publishing ', '2017-01-10', 9, 'G001'),
-('B004', 'Idiot Genius', '\"From its engaging drawings to its powerful message, Idiot Genius will leave readers musing about Willa Snap', 70, 'Richard Due', 'Gibbering Gnome Press', '2017-12-01', 3, 'G002'),
-('B005', 'The Riddle in Stone Trilogy', 'In the world of fantasy novels, Robert Evert is a supernova talent, and the RIDDLE IN STONE series brings twisting suspense and an unforgettable protagonist into the pantheon of great books.', 67, 'Robert Evert', 'Diversion Books', '2017-12-25', 22, 'G002'),
-('B006', 'Undone', 'USA Today and New York Times bestselling author, Wendy Higgins, brings you the gripping, sensual conclusion to her apocalyptic trilogy.', 45, 'Wendy Higgins ', 'A Division of Ingenious Inventions Run Amok, Ink', '2018-01-01', 56, 'G002'),
-('B007', 'The Hunger Game', 'The nation of Panem, formed from a post-apocalyptic North America, is a country that consists of a wealthy Capitol region surrounded by 12 poorer districts.', 87, 'Suzanne Collins', 'Scholastic Press', '2018-01-05', 54, 'G003'),
-('B008', 'The Maze Runner', 'When Thomas wakes up in the lift, the only thing he can remember is his name. ', 67, ' James Dashner', 'Delacorte Press', '2018-01-13', 25, 'G003'),
-('B009', 'The Lightning Thief ', 'Percy Jackson is a good kid, but he can', 56, ' Rick Riordan', 'Disney Hyperion Books', '2018-01-20', 27, 'G003');
+('B001', 'Milk and Honey', 'It is about the experience of violence, abuse, love, loss, and femininity.', 20, 'Rupi Kaur', 'Createspace', '4th November 2014 ', 10, 'G001'),
+('B002', 'Pride and Prejudice', '“It is a truth universally acknowledged, that a single man in possession of a good fortune must be in want of a wife.” So begins Pride and Prejudice, Jane Austen’s witty comedy of manners', 30, ' Jane Austen', 'Modern Library', '10th October 2000', 13, 'G001'),
+('B003', 'Memories of Geisha', 'A literary sensation and runaway bestseller, this brilliant debut novel presents with seamless authenticity and exquisite', 15, 'Arthur Golden', 'Random House Large Print Publishing ', '15th November 2005', 9, 'G001'),
+('B004', 'Idiot Genius', '\"From its engaging drawings to its powerful message, Idiot Genius will leave readers musing about Willa Snap\'s adventures long after the winding story concludes.', 70, 'Richard Due', 'Gibbering Gnome Press', '22nd December 2017', 20, 'G002'),
+('B005', 'The Riddle in Stone Trilogy', 'In the world of fantasy novels, Robert Evert is a supernova talent, and the RIDDLE IN STONE series brings twisting suspense and an unforgettable protagonist into the pantheon of great books.', 67, 'Robert Evert', 'Diversion Books', '7th December 2014', 36, 'G002'),
+('B006', 'Undone', 'USA Today and New York Times bestselling author, Wendy Higgins, brings you the gripping, sensual conclusion to her apocalyptic trilogy.\r\n', 45, 'Wendy Higgins ', 'A Division of Ingenious Inventions Run Amok, Ink', '5th December 2017', 56, 'G002'),
+('B007', 'The Hunger Game', 'The nation of Panem, formed from a post-apocalyptic North America, is a country that consists of a wealthy Capitol region surrounded by 12 poorer districts.', 87, 'Suzanne Collins', 'Scholastic Press', '14th September 2008', 70, 'G003'),
+('B008', 'The Maze Runner', 'When Thomas wakes up in the lift, the only thing he can remember is his name. ', 67, ' James Dashner', 'Delacorte Press', '6th October 2009', 40, 'G003'),
+('B009', 'The Lightning Thief ', 'Percy Jackson is a good kid, but he can\'t seem to focus on his schoolwork or control his temper.', 56, ' Rick Riordan', 'Disney Hyperion Books', '1st March 2006', 36, 'G003');
 
 -- --------------------------------------------------------
 
@@ -61,10 +64,13 @@ INSERT INTO `book` (`bookId`, `bookTitle`, `bookDescription`, `bookPrice`, `book
 -- Table structure for table `bookpurchase`
 --
 
-CREATE TABLE `bookpurchase` (
+DROP TABLE IF EXISTS `bookpurchase`;
+CREATE TABLE IF NOT EXISTS `bookpurchase` (
   `paymentId` varchar(10) NOT NULL,
   `bookId` varchar(10) NOT NULL,
-  `purchaseQuantity` int(11) NOT NULL
+  `purchaseQuantity` int(11) NOT NULL,
+  PRIMARY KEY (`paymentId`,`bookId`),
+  KEY `bookId` (`bookId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -73,14 +79,18 @@ CREATE TABLE `bookpurchase` (
 -- Table structure for table `feedback`
 --
 
-CREATE TABLE `feedback` (
+DROP TABLE IF EXISTS `feedback`;
+CREATE TABLE IF NOT EXISTS `feedback` (
   `feedbackId` varchar(10) NOT NULL,
   `bookRating` int(11) NOT NULL,
   `bookComment` varchar(255) NOT NULL,
   `feedbackStatus` tinyint(1) NOT NULL DEFAULT '1',
   `feedbackDateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `memberId` varchar(10) NOT NULL,
-  `bookId` varchar(10) NOT NULL
+  `bookId` varchar(10) NOT NULL,
+  PRIMARY KEY (`feedbackId`),
+  KEY `memberId` (`memberId`),
+  KEY `bookId` (`bookId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -89,11 +99,14 @@ CREATE TABLE `feedback` (
 -- Table structure for table `feedbackrating`
 --
 
-CREATE TABLE `feedbackrating` (
+DROP TABLE IF EXISTS `feedbackrating`;
+CREATE TABLE IF NOT EXISTS `feedbackrating` (
   `memberId` varchar(10) NOT NULL,
   `feedbackId` varchar(10) NOT NULL,
   `feedbackRating` int(11) NOT NULL,
-  `feedbackRatingDateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `feedbackRatingDateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`memberId`,`feedbackId`),
+  KEY `feedbackId` (`feedbackId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -102,9 +115,11 @@ CREATE TABLE `feedbackrating` (
 -- Table structure for table `genre`
 --
 
-CREATE TABLE `genre` (
+DROP TABLE IF EXISTS `genre`;
+CREATE TABLE IF NOT EXISTS `genre` (
   `genreId` varchar(4) NOT NULL,
-  `genre` varchar(20) NOT NULL
+  `genre` varchar(20) NOT NULL,
+  PRIMARY KEY (`genreId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -122,13 +137,15 @@ INSERT INTO `genre` (`genreId`, `genre`) VALUES
 -- Table structure for table `manager`
 --
 
-CREATE TABLE `manager` (
+DROP TABLE IF EXISTS `manager`;
+CREATE TABLE IF NOT EXISTS `manager` (
   `managerId` varchar(10) NOT NULL,
   `managerEmail` varchar(255) NOT NULL,
   `managerName` varchar(100) NOT NULL,
   `managerPw` varchar(255) NOT NULL,
   `managerAddress` varchar(255) NOT NULL,
-  `managerPhone` varchar(15) NOT NULL
+  `managerPhone` varchar(15) NOT NULL,
+  PRIMARY KEY (`managerId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -144,7 +161,8 @@ INSERT INTO `manager` (`managerId`, `managerEmail`, `managerName`, `managerPw`, 
 -- Table structure for table `member`
 --
 
-CREATE TABLE `member` (
+DROP TABLE IF EXISTS `member`;
+CREATE TABLE IF NOT EXISTS `member` (
   `memberId` varchar(10) NOT NULL,
   `memberEmail` varchar(255) NOT NULL,
   `memberName` varchar(100) NOT NULL,
@@ -152,17 +170,23 @@ CREATE TABLE `member` (
   `memberAddress` varchar(255) NOT NULL,
   `memberPhone` varchar(15) NOT NULL,
   `memberCreditCard` varchar(255) NOT NULL,
-  `memberTrust` tinyint(1) DEFAULT NULL,
-  `memberPoint` int(11) NOT NULL DEFAULT '0'
+  `memberTrust` tinyint(1) NOT NULL DEFAULT '0',
+  `memberPoint` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`memberId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `member`
+-- Table structure for table `memberlogin`
 --
 
-INSERT INTO `member` (`memberId`, `memberEmail`, `memberName`, `memberPw`, `memberAddress`, `memberPhone`, `memberCreditCard`, `memberTrust`, `memberPoint`) VALUES
-('M001', 'abc@gmail.com', 'Tuck', '$2y$10$YswrBp1kku27Cx4aUc5TwOt96XWBlCBCURXSgWvG9NUTtpaEPMvoe', 'Subang', '0123456789', '123123141224', 0, 0),
-('M002', 'tan@gmail.com', 'Tan', '$2y$10$j7xx0zs.bwv6uuCE0OyMLetOtxEJmNdwy5zt4tlMchjhA2pPHd7O2', 'Puchong', '1234214', '121213132', 0, 0);
+DROP TABLE IF EXISTS `memberlogin`;
+CREATE TABLE IF NOT EXISTS `memberlogin` (
+  `memberId` varchar(10) NOT NULL,
+  `loginDateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`memberId`,`loginDateTime`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -170,11 +194,14 @@ INSERT INTO `member` (`memberId`, `memberEmail`, `memberName`, `memberPw`, `memb
 -- Table structure for table `memberreward`
 --
 
-CREATE TABLE `memberreward` (
+DROP TABLE IF EXISTS `memberreward`;
+CREATE TABLE IF NOT EXISTS `memberreward` (
   `memberId` varchar(10) NOT NULL,
   `rewardId` varchar(10) NOT NULL,
   `claimDateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `claimQuantity` int(11) NOT NULL
+  `claimQuantity` int(11) NOT NULL,
+  PRIMARY KEY (`memberId`,`rewardId`),
+  KEY `rewardId` (`rewardId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -183,10 +210,13 @@ CREATE TABLE `memberreward` (
 -- Table structure for table `payment`
 --
 
-CREATE TABLE `payment` (
+DROP TABLE IF EXISTS `payment`;
+CREATE TABLE IF NOT EXISTS `payment` (
   `paymentId` varchar(10) NOT NULL,
   `paymentDateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `memberId` varchar(10) NOT NULL
+  `memberId` varchar(10) NOT NULL,
+  PRIMARY KEY (`paymentId`),
+  KEY `memberId` (`memberId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -195,12 +225,14 @@ CREATE TABLE `payment` (
 -- Table structure for table `reward`
 --
 
-CREATE TABLE `reward` (
+DROP TABLE IF EXISTS `reward`;
+CREATE TABLE IF NOT EXISTS `reward` (
   `rewardId` varchar(10) NOT NULL,
   `rewardName` varchar(255) NOT NULL,
   `rewardDescription` varchar(255) NOT NULL,
   `rewardPoint` int(11) NOT NULL,
-  `rewardQuantity` int(11) NOT NULL
+  `rewardQuantity` int(11) NOT NULL,
+  PRIMARY KEY (`rewardId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -209,7 +241,7 @@ CREATE TABLE `reward` (
 
 INSERT INTO `reward` (`rewardId`, `rewardName`, `rewardDescription`, `rewardPoint`, `rewardQuantity`) VALUES
 ('R001', 'RM10 Book Voucher', 'RM10 discount of books purchase ', 250, 10),
-('R002', 'RM20 Book Voucher', 'RM20 discount of books purchase', 400, 25);
+('R002', 'RM20 Book Voucher', 'RM20 discount of books purchase', 400, 5);
 
 -- --------------------------------------------------------
 
@@ -217,13 +249,17 @@ INSERT INTO `reward` (`rewardId`, `rewardName`, `rewardDescription`, `rewardPoin
 -- Table structure for table `stockorder`
 --
 
-CREATE TABLE `stockorder` (
+DROP TABLE IF EXISTS `stockorder`;
+CREATE TABLE IF NOT EXISTS `stockorder` (
   `orderId` varchar(10) NOT NULL,
   `orderDate` date NOT NULL,
   `orderStatus` varchar(20) NOT NULL,
   `arrivalDate` date NOT NULL,
   `managerId` varchar(10) NOT NULL,
-  `supplierId` varchar(10) NOT NULL
+  `supplierId` varchar(10) NOT NULL,
+  PRIMARY KEY (`orderId`),
+  KEY `managerId` (`managerId`),
+  KEY `supplierId` (`supplierId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -232,10 +268,13 @@ CREATE TABLE `stockorder` (
 -- Table structure for table `stockorderinfo`
 --
 
-CREATE TABLE `stockorderinfo` (
+DROP TABLE IF EXISTS `stockorderinfo`;
+CREATE TABLE IF NOT EXISTS `stockorderinfo` (
   `orderId` varchar(10) NOT NULL,
   `bookId` varchar(10) NOT NULL,
-  `orderQuantity` int(11) NOT NULL
+  `orderQuantity` int(11) NOT NULL,
+  PRIMARY KEY (`orderId`,`bookId`),
+  KEY `bookId` (`bookId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -244,11 +283,13 @@ CREATE TABLE `stockorderinfo` (
 -- Table structure for table `supplier`
 --
 
-CREATE TABLE `supplier` (
+DROP TABLE IF EXISTS `supplier`;
+CREATE TABLE IF NOT EXISTS `supplier` (
   `supplierId` varchar(10) NOT NULL,
   `supplierName` varchar(100) NOT NULL,
   `supplierAddress` varchar(255) NOT NULL,
-  `suplierPhone` varchar(15) NOT NULL
+  `suplierPhone` varchar(15) NOT NULL,
+  PRIMARY KEY (`supplierId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -259,98 +300,6 @@ INSERT INTO `supplier` (`supplierId`, `supplierName`, `supplierAddress`, `suplie
 ('S001', 'Adrian Wong', 'Shah Alam', '03-89765678'),
 ('S002', 'Emeline Cheah', 'Serdang', '012-7768790'),
 ('S003', 'Aaron', 'Kajang', '03-89764544');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `book`
---
-ALTER TABLE `book`
-  ADD PRIMARY KEY (`bookId`),
-  ADD KEY `book_ibfk_1` (`genreId`);
-
---
--- Indexes for table `bookpurchase`
---
-ALTER TABLE `bookpurchase`
-  ADD PRIMARY KEY (`paymentId`,`bookId`),
-  ADD KEY `bookId` (`bookId`);
-
---
--- Indexes for table `feedback`
---
-ALTER TABLE `feedback`
-  ADD PRIMARY KEY (`feedbackId`),
-  ADD KEY `memberId` (`memberId`),
-  ADD KEY `bookId` (`bookId`);
-
---
--- Indexes for table `feedbackrating`
---
-ALTER TABLE `feedbackrating`
-  ADD PRIMARY KEY (`memberId`,`feedbackId`),
-  ADD KEY `feedbackId` (`feedbackId`);
-
---
--- Indexes for table `genre`
---
-ALTER TABLE `genre`
-  ADD PRIMARY KEY (`genreId`);
-
---
--- Indexes for table `manager`
---
-ALTER TABLE `manager`
-  ADD PRIMARY KEY (`managerId`);
-
---
--- Indexes for table `member`
---
-ALTER TABLE `member`
-  ADD PRIMARY KEY (`memberId`);
-
---
--- Indexes for table `memberreward`
---
-ALTER TABLE `memberreward`
-  ADD PRIMARY KEY (`memberId`,`rewardId`),
-  ADD KEY `rewardId` (`rewardId`);
-
---
--- Indexes for table `payment`
---
-ALTER TABLE `payment`
-  ADD PRIMARY KEY (`paymentId`),
-  ADD KEY `memberId` (`memberId`);
-
---
--- Indexes for table `reward`
---
-ALTER TABLE `reward`
-  ADD PRIMARY KEY (`rewardId`);
-
---
--- Indexes for table `stockorder`
---
-ALTER TABLE `stockorder`
-  ADD PRIMARY KEY (`orderId`),
-  ADD KEY `managerId` (`managerId`),
-  ADD KEY `supplierId` (`supplierId`);
-
---
--- Indexes for table `stockorderinfo`
---
-ALTER TABLE `stockorderinfo`
-  ADD PRIMARY KEY (`orderId`,`bookId`),
-  ADD KEY `bookId` (`bookId`);
-
---
--- Indexes for table `supplier`
---
-ALTER TABLE `supplier`
-  ADD PRIMARY KEY (`supplierId`);
 
 --
 -- Constraints for dumped tables
@@ -382,6 +331,12 @@ ALTER TABLE `feedback`
 ALTER TABLE `feedbackrating`
   ADD CONSTRAINT `feedbackrating_ibfk_1` FOREIGN KEY (`memberId`) REFERENCES `member` (`memberId`),
   ADD CONSTRAINT `feedbackrating_ibfk_2` FOREIGN KEY (`feedbackId`) REFERENCES `feedback` (`feedbackId`);
+
+--
+-- Constraints for table `memberlogin`
+--
+ALTER TABLE `memberlogin`
+  ADD CONSTRAINT `memberlogin_ibfk_1` FOREIGN KEY (`memberId`) REFERENCES `member` (`memberId`);
 
 --
 -- Constraints for table `memberreward`
